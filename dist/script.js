91366a11a1550f1012d90586ef5253d61658de54
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2023-01-08 08:10:19 -0500

    Add high level review

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,15 @@
+const statusBar = document.querySelector("#status-bar");
+const statusBarControl = document.querySelector(".status-bar-control");
 
+statusBarControl.addEventListener("click", function (e) {
+  const button = e.target.closest("button");
+  if (!button) return;
+
+  const className = button.dataset.btnStatus;
+
+  const currentClass = statusBar.className;
+
+  if (currentClass) {
+    statusBar.classList.replace(currentClass, className);
+  }
+});
